c4d3ca562d01c1eed49277212db0af511b8e483a
Shinan Tc <shinantc1@gmail.com>
2026-05-06 12:39:03 +0530

    unicode error fixed. emojis are not accepted

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from pipeline import run_pipeline
+import sys
+import io
+
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
 
 if __name__ == "__main__":
     run_pipeline()
